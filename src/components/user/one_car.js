8b2d1f01214f382b9177c659910c7868b670e7bd
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-12 11:40:31 -0700

    single car displaying

--- src/components/user/one_car.js
+++ src/components/user/one_car.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
 import Img from 'react-image'
+import { Table } from 'reactstrap';
 
 class OneCar extends Component {
   render(){
@@ -12,7 +13,9 @@ class OneCar extends Component {
     let model = this.props.car ? this.props.car.model : null;
     let trim = this.props.car ? this.props.car.trim : null;
     let image = this.props.car ? this.props.car.link : null;
-
+    let price = this.props.car ? this.props.car.price : null;
+    let sale = this.props.car ? this.props.car.sale : null;
+    let g66price = price - sale/100;
 
     return (
       <Container>
@@ -27,15 +30,41 @@ class OneCar extends Component {
         </Row>
         <div className="container">
           <Row className="image-n-price">
-            <Col md="7">
+            <Col md="8">
               <Img className="singleImage" src={image} />
             </Col>
-            <Col md="5">
+            <Col md="4">
               <div className="one-car-price">
                 <h4>Detail Pricing</h4>
-                <div>
+                <div className="price">
                    <Table size="sm">
-                     
+                     <tbody>
+                      <tr>
+                        <td>MSRP:</td>
+                        <td>${price}</td>
+                      </tr>
+                      <tr>
+                        <td>DEALER'S DISCOUNT:</td>
+                        <td>{sale}%</td>
+                      </tr>
+                      <tr>
+                        <td>G66 CAR PRICE</td>
+                        <td>{g66price}</td>
+                      </tr>
+                    </tbody>
+                   </Table>
+                   <hr/>
+                   <Table size="sm">
+                     <tbody>
+                      <tr>
+                        <td>Estimated Lease:</td>
+                        <td>$199/pm**</td>
+                      </tr>
+                      <tr>
+                        <td>Estimated Finance:</td>
+                        <td>$250/pm**</td>
+                      </tr>
+                    </tbody>
                    </Table>
                 </div>
               </div>
@@ -45,10 +74,10 @@ class OneCar extends Component {
 
 
         <Row>
-          <Col md="7">
+          <Col md="8">
             detail features
           </Col>
-          <Col md="5">
+          <Col md="4">
             Schedule Test Drive
           </Col>
         </Row>
