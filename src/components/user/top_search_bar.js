c1d4effba2273e2da46eaa0ff1bf914bc68182cf
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-11 18:54:08 -0700

    search bar works

--- src/components/user/top_search_bar.js
+++ src/components/user/top_search_bar.js
@@ -12,79 +12,92 @@ class TopSearchBar extends Component {
 
   render(){
 
-    let seachYear = 2018;
+
     let searchMake = ["Audi","Honda","Hundai","Lamborghini","Maserati", "Subaru","Toyota"];
 
-    // Audi
-    let audiModel = ["A3","Q3","A5"];
-    let audiBody = ["Sedan","Coupe","SUV"];
-    let audiTrim = ["2.OT","2.OT","2.OT"];
-    // Honda
-    let hondaModel = ["Accord","Civic","Odyssey","CR-V","HR-V"];
-    let hondaBody = ["Sedan","Hatchback","Van","SUV"];
-    let hondaTrim = ["LX"];
-    // Lamborghini
-    let lambModel = ["Aventador"];
-    let lambBody = ["Coupe"];
-    let lambTrim = ["SVRoadster"];
-    // Maserati
-    let maseratiModel = ["GranTurismo", "Levante"];
-    let maseratiBody = ["Coupe", "SUV"];
-    let maseratiTrim = ["Sports","S GranLusso"];
-    // Mercedes-benz
-    let mercedesModel = ["C-Class"];
-    let mercedesBody = ["Sedan","Convertible","Coupe"];
-    let mercedesTrim = ["C300","AMG-C43"];
-    // Subaru
-    let subaruModel = ["Outback"];
-    let subaruBody = ["Hatchback"];
-    let subaruTrim = ["2.5i"];
-    // Toyota
-    let toyotaModel = ["4Runner","Avalon","Camry","Tocoma","Tundra"];
-    let toyotaBody = ["SUV","Sedan","Truck"];
-    let toyotaTrim = ["SR5","XLE","LE","SR"];
+    // // Audi
+    // let audiModel = ["A3","Q3","A5"];
+    // let audiBody = ["Sedan","Coupe","SUV"];
+    // let audiTrim = ["2.OT","2.OT","2.OT"];
+    // // Honda
+    // let hondaModel = ["Accord","Civic","Odyssey","CR-V","HR-V"];
+    // let hondaBody = ["Sedan","Hatchback","Van","SUV"];
+    // let hondaTrim = ["LX"];
+    // // Lamborghini
+    // let lambModel = ["Aventador"];
+    // let lambBody = ["Coupe"];
+    // let lambTrim = ["SVRoadster"];
+    // // Maserati
+    // let maseratiModel = ["GranTurismo", "Levante"];
+    // let maseratiBody = ["Coupe", "SUV"];
+    // let maseratiTrim = ["Sports","S GranLusso"];
+    // // Mercedes-benz
+    // let mercedesModel = ["C-Class"];
+    // let mercedesBody = ["Sedan","Convertible","Coupe"];
+    // let mercedesTrim = ["C300","AMG-C43"];
+    // // Subaru
+    // let subaruModel = ["Outback"];
+    // let subaruBody = ["Hatchback"];
+    // let subaruTrim = ["2.5i"];
+    // // Toyota
+    // let toyotaModel = ["4Runner","Avalon","Camry","Tocoma","Tundra"];
+    // let toyotaBody = ["SUV","Sedan","Truck"];
+    // let toyotaTrim = ["SR5","XLE","LE","SR"];
 
-    let makes = searchMake.map(make => {
-      return (<option key={make} value={make}>{make}</option>)
-    })
+
 
     console.log('local state: ', this.state);
-
+    let seachYear = 2018;
     let cars = {
+      Audi: [
+        {model: "A3", body: "Sedan", trim: '2.OT'},
+        {model: "Q3", body: "Coupe", trim: '2.OT'},
+        {model: "A5", body: "SUV", trim: '2.OT'}
+      ],
       Honda: [
-        {
-          model: "Accord",
-          body: "Sedan",
-          trim: 'LX'
-        },
-        {
-          model: "Civic",
-          body: "Sedan",
-          trim: 'LX'
-        },
-        {
-          model: "Odyssey",
-          body: "Van",
-          trim: 'LX'
-        },
-        {
-          model: "CR-V",
-          body: "SUV",
-          trim: 'LX'
-        },
-        {
-          model: "HR-V",
-          body: "SUV",
-          trim: 'LX'
-        }
+        {model: "Accord", body: "Sedan", trim: 'LX'},
+        {model: "Civic", body: "Sedan", trim: 'LX'},
+        {model: "Odyssey", body: "Van", trim: 'LX'},
+        {model: "CR-V", body: "SUV", trim: 'LX'},
+        {model: "HR-V", body: "SUV", trim: 'LX'}
+      ],
+      Lamborghini: [
+        {model: "Aventador", body: "Coupe", trim: 'SVRoadster'}
+      ],
+      Maserati: [
+        {model: "GranTurismo", body: "Coupe", trim: 'Sports'},
+        {model: "Levante", body: "SUV", trim: 'S GranLusso'},
+      ],
+      Mercedes: [
+        {model: "C-Class", body: "Sedan", trim: 'C300'},
+        {model: "C-Class", body: "Convertible", trim: 'AMG-C43'},
+        {model: "C-Class", body: "Coupe", trim: 'C300'}
+      ],
+      Subaru: [
+        {model: "Outback", body: "Hatchback", trim: '2.5i'}
+      ],
+      Toyota: [
+        {model: "4Runner", body: "SUV", trim: 'SR5'},
+        {model: "Avalon", body: "Sedan", trim: 'XLE'},
+        {model: "Camry", body: "Sedan", trim: 'LE'},
+        {model: "Tocoma", body: "Truck", trim: 'SR'},
+        {model: "Tundra", body: "Truck", trim: 'SR'}
       ]
     }
 
-    if(this.state.make == "Honda") {
-      var models = hondaModel.map(model => {
+    let makes = searchMake.map((make, i) => {
+      return (
+        <option key={i} value={make}>
+          {this.state.year ? make : ''}
+        </option>
+      )
+    })
+
+    if(this.state.make) {
+      var models = cars[this.state.make].map((make, i) => {
         return (
-          <option key={model} value={model}>
-              {model}
+          <option key={i} value={make.model}>
+              {make.model}
           </option>)
       })
     }
@@ -97,6 +110,7 @@ class TopSearchBar extends Component {
           </option>)
       })
     }
+
     if(this.state.body) {
       var trim = cars[this.state.make].map(car => {
         return (
@@ -116,7 +130,7 @@ class TopSearchBar extends Component {
                     value={this.state.year}
                     onChange={(e)=>this.setState({year: e.target.value})}
                   >
-                  <option disabled selected>Select Year</option>
+                  <option defaultValue>Select Year</option>
                   <option>{seachYear}</option>
                 </select>
               </div>
@@ -125,8 +139,8 @@ class TopSearchBar extends Component {
                     value={this.state.make}
                     onChange={(e)=>this.setState({make: e.target.value})}
                   >
-                  <option disabled selected>Select Make</option>
-                  {makes}
+                  <option defaultValue>Select Make</option>
+                    {makes}
                 </select>
               </div>
               <div className="col-2">
@@ -134,7 +148,7 @@ class TopSearchBar extends Component {
                   value={this.state.model}
                   onChange={(e)=>this.setState({model: e.target.value})}
                   >
-                  <option disabled selected>Select Model</option>
+                  <option defaultValue>Select Model</option>
                     { models ? models : '' }
                 </select>
               </div>
@@ -143,7 +157,7 @@ class TopSearchBar extends Component {
                   value={this.state.body}
                   onChange={(e)=>this.setState({body: e.target.value})}
                   >
-                  <option disabled selected>Select Body</option>
+                  <option defaultValue>Select Body</option>
                     { body ? body : '' }
                 </select>
               </div>
@@ -152,7 +166,7 @@ class TopSearchBar extends Component {
                   value={this.state.trim}
                   onChange={(e)=>this.setState({trim: e.target.value})}
                   >
-                  <option disabled selected>Select Trim</option>
+                  <option defaultValue>Select Trim</option>
                     { trim ? trim : '' }
                 </select>
               </div>
